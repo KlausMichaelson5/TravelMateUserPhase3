c21eddc784c4d8a978235f847a95778b2c215acf
KlausMichaelson5 <raghuc2001@gmail.com>
2024-08-26 20:01:21 +0530

    admin added

---
+++ AdminMvcUi/Controllers/AdminUiController.cs
@@ -0,0 +1,29 @@
+﻿using AdminMvcUi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AdminMvcUi.Controllers
+{
+    public class AdminUiController : Controller
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminUiController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _adminService.GetAll();
+            return View(users);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _adminService.DeleteUser(id);
+            return Ok();
+        }
+    }
+}
